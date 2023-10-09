01369061312158f1e93f24ca1bafeb6b51a39992
DeibyAlexander <deiby2707@gmail.com>
2023-10-09 15:15:41 -0500

    feat: :zap: Progreso en los estilos del login

---
+++ frontend/src/components/FondoLogin.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../assets/css/login.css'
+
+const FondoLogin = () => {
+  return (
+    <div className='primer-fondo'>
+        <div className='segundo-fondo'>
+          <div className='card-login'>
+              <div className='container-login'>
+               
+              </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+
+export default FondoLogin

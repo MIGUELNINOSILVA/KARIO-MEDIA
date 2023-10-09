da5e1a0e808272d9bdb08db001c394b15433c2b2
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-10-09 18:54:18 -0500

    feat: :sparkles: Validación de token para poder acceder a los endpoints

--- backend/src/middlewares/authJwt.js
+++ backend/src/middlewares/authJwt.js
@@ -1,4 +1,16 @@
-export const verifyToken = async(req, res, next)=>{
- const token = req.headers["x-access-token"];
- console.log(token);
-}
+import jwt from "jsonwebtoken";
+import Usuario from "../models/Usuario.js";
+
+export const verifyToken = async (req, res, next) => {
+  const token = req.headers["x-access-token"];
+  if (!token)
+    return res.status(403).json({ message: "No se ha proporcionado un token" });
+
+  const decoded = jwt.verify(token, process.env.SECRET);
+  req.userId = decoded.id;
+
+  const user = await Usuario.findById(req.userId, { contraseña: 0 });
+  if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
+
+  next();
+};
--- backend/src/routes/rol.routes.js
+++ backend/src/routes/rol.routes.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 const router = Router();
 
 import { getRoles } from "../controllers/rol.controller.js";
+import { verifyToken } from "../middlewares/authJwt.js";
 
-router.get("/", getRoles);
+router.get("/", verifyToken, getRoles);
 
 export default router;

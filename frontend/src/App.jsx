8e55120ee5e9e2523ab957af8f77b5789d54330c
JoseK03 <dajoseva@gmail.com>
2023-10-09 16:19:01 -0500

    feat: :wrench: Intalacion de material UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import login from './pages/login'
+import Error404 from './pages/Error404';
 
 export const App = () => {
   return (
@@ -8,7 +9,7 @@ export const App = () => {
       <BrowserRouter>
         <Routes>
             <Route exact path='/'  Component={login}></Route>
-            <Route path='*' Component={<Error404/>}></Route> {/*//! Esta ruta debe estar de ultimo!! */}
+            <Route exact path='*' Component={Error404}></Route> {/*//! Esta ruta debe estar de ultimo!! */}
         </Routes>
       </BrowserRouter>
     </>

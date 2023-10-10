5197736e5af6546d9acf3843a55addad415cf7d6
JoseK03 <dajoseva@gmail.com>
2023-10-10 07:10:50 -0500

    feat: :white_check_mark: Creacion BotonAAyuda
    Se crea componente para el boton agregar ayuda y se reduce codigo dentro de pagina help

---
+++ frontend/src/components/BotonAAyuda.jsx
@@ -0,0 +1,53 @@
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import { colors } from '@mui/material';
+export default function BotonAAyuda() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+   <>
+     <div style={{display:'flex',flexDirection:'row-reverse', margin:'10px',border:'#FC7124'}}>
+      <Button variant="outlined" onClick={handleClickOpen} style={{color:'#FC7124'}}>
+        Agregar Ayuda
+      </Button>
+        <form action="">
+        <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Inserta tu solicitud de ayuda"}
+        </DialogTitle>
+        <DialogContent>
+        
+        <label htmlFor="ayuda">Describe tu solicitud</label>
+        <input type="text" placeholder='Necesito ayuda urgente con ...' />
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} style={{color:'#FC7124'}}>Cancelar</Button>
+          <Button onClick={handleClose} autoFocus style={{color:'#FC7124'}}>Añadir</Button>
+        </DialogActions>
+      </Dialog>
+        </form>
+    </div>
+   </>
+  );
+}

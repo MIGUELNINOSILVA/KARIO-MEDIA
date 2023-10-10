2945a5c5afd5ace986f6f6868213f3f593ef4061
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-10-10 13:46:08 -0500

    feat: :construction: Trabajando el contenido principal

---
+++ frontend/src/components/PrincipalContent.jsx
@@ -0,0 +1,18 @@
+export const PrincipalContent = () => {
+  return (
+    <>
+      <div className="content-indicadores">
+        <h1>Panel de indicadores</h1>
+        <p>
+          Aqui puedes visualizar los indicadores propuestos y añadidos por tu
+          equipo de trabajo. Si quieres ver más detalles, dale click a uno de
+          ellos para obtener más información.
+        </p>
+      </div>
+      <div className="content-indicadores">
+        
+      </div>
+      <div className="content-indicadores"></div>
+    </>
+  );
+};

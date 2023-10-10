633fd85d06bd19a745900f6fc58ff1cd84537d37
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-10-09 19:29:11 -0500

    feat: :sparkles: LOGIN

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ export const App = () => {
       <BrowserRouter>
         <Routes>
             <Route exact path='/login' Component={FondoLogin}></Route>
+            
         </Routes>
       </BrowserRouter>
     </>

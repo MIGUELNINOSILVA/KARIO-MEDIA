eb8e0b7be49fa545ff1273dfe44e3bec8e5430bb
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-10-09 21:04:26 -0500

    feat: :bug: Cambios realizados del merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import FondoLogin from './pages/FondoLogin'
-import login from './pages/login'
+// import login from './pages/login'
 import Error404 from './pages/Error404';
 import Principal from './pages/Principal';
 import help from './pages/help';
@@ -12,7 +12,7 @@ export const App = () => {
       <BrowserRouter>
         <Routes>
             <Route exact path='/login' Component={FondoLogin}></Route>    
-            <Route exact path='/'  Component={login}></Route>
+            {/* <Route exact path='/'  Component={login}></Route> */}
             <Route exact path='/Kario' Component={Principal}></Route>
             <Route exact path='/help' Component={help}></Route>
             <Route exact path='*' Component={Error404}></Route> {/*//! Esta ruta debe estar de ultimo!! */}

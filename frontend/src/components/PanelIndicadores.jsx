8266359b0ba6249cf7d6330b3fdb1f096423ac63
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-10-11 07:59:36 -0500

    feat: :sparkles: Drag and Drop funcional front

---
+++ frontend/src/components/PanelIndicadores.jsx
@@ -0,0 +1,10 @@
+
+export const PanelIndicadores = () => {
+  return (
+    <>
+      <h1>Panel de indicadores</h1>
+      <p>Aquí puedes visualizar los indicadores propuestos y añadidios por tu equipo de trabajo. Si quieres ver más detalles, dale click a uno de ellos para más información.</p>
+      
+    </>
+  )
+}

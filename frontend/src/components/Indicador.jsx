741a47c4f727f58517dd4dfe5c51be1e2959458d
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-10-11 10:59:36 -0500

    feat: :sparkles: Porcentaje de cummplimiento funcional

--- frontend/src/components/Indicador.jsx
+++ frontend/src/components/Indicador.jsx
@@ -1,4 +1,12 @@
 import { useSortable } from "@dnd-kit/sortable";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Box,
+  ChakraProvider,
+  CircularProgress,
+  CircularProgressLabel,
+} from "@chakra-ui/react";
+import "./../assets/css/Indicador.css";
 
 export const Indicador = ({ user }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
@@ -11,17 +19,55 @@ export const Indicador = ({ user }) => {
     transition,
   };
 
+  // Establece estilos en línea para aplicar colores
+  const cellStyle = {
+    backgroundColor: "white", // Cambia esto al color de fondo deseado
+    color: "black", // Cambia esto al color de texto deseado
+  };
+
+  let valor = 55;
+
   return (
     <tr
       ref={setNodeRef}
       {...attributes}
       {...listeners}
       style={style}
-      className="indicador-box"
+      className="td-indicador"
     >
-      <td>{user.name}</td>
-      <td>{user.name}</td>
-      {/* Agrega más celdas td y estilos según sea necesario */}
+      <td style={cellStyle}>{user.name}</td>
+      <td style={cellStyle}>{user.name}</td>
+      <td style={cellStyle}>{user.name}</td>
+      <td style={cellStyle}>{user.name}</td>
+      <td style={cellStyle}>{user.name}</td>
+      <td style={cellStyle}>{user.name}</td>
+      <td style={cellStyle}>{user.name}</td>
+      <td style={cellStyle}>
+        <ChakraProvider>
+          <Box>
+            <CircularProgress
+              value={valor}
+              color={
+                valor > 0 && valor < 40
+                  ? "red"
+                  : valor >= 40 && valor < 80
+                  ? "orange"
+                  : valor >= 80
+                  ? "#00ff40"
+                  : ""
+              }
+              size="40px"
+              thickness="12px"
+            >
+              <CircularProgressLabel>{valor}%</CircularProgressLabel>
+            </CircularProgress>
+          </Box>
+        </ChakraProvider>
+      </td>
+      <td style={cellStyle}>{user.name}</td>
+      <td style={cellStyle}>
+        <MenuIcon />
+      </td>
     </tr>
   );
 };

3517e4681901ea5c4e8671f578682715e2c0cef8
DeibyAlexander <deiby2707@gmail.com>
2023-10-12 00:52:11 -0500

    feat: :lipstick: DIseño de imagen de carga con consumo en metodo get de Reportes

--- frontend/src/components/Reportes.jsx
+++ frontend/src/components/Reportes.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import ModalReportes from './ModalReportes'
 import usu from './../assets/img/VectorUsuario.svg'
 import '../assets/css/reportes.css'
+import loadingImage from '../assets/img/KARIO_LOGO.png'
+import '../assets/css/cargar.css'
 
 const listaReportes = [
   {
@@ -10,8 +12,46 @@ const listaReportes = [
   }
 ]
 const Reportes = () => {
+  const url = 'http://localhost:4000/api/reporte'
 
   const [listado, setListado] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [reportes, setReportes] = useState([])
+  
+  
+
+  useEffect(() => {
+    // Realiza la carga de datos aquí
+    getReportes()
+      .then(() => {
+        setLoading(false); // Una vez que los datos se cargan, establece isLoading en false
+      })
+      .catch((error) => {
+        console.error('Error en la solicitud:', error);
+        show_alerta('Error en la solicitud');
+        setLoading(false); // Asegúrate de cambiar el estado incluso en caso de error
+      });
+  }, []);
+
+  
+const getReportes = async () => {
+    try {
+      const respuesta = await fetch(url);
+      
+      const datos = await respuesta.json();
+      console.log(datos);
+      setReportes(datos)
+      
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+    }
+  }
+  
+  useEffect(()=>{
+    getReportes()
+},[])
+
+
 
   useEffect(()=>{
     const listaGuardada = JSON.parse(localStorage.getItem('listaReportes'));
@@ -27,13 +67,21 @@ const Reportes = () => {
 
   return (
     <>
+      {isLoading ? (
+      <div className="cargando" >
+          <img src={loadingImage} alt="Loading" className="loading-image" />        
+      </div>     
+        ) : (
+            <>
+            </>
+        )}
       <ModalReportes nuevoElemento={nuevoElemento} listaReportes={listado}/>
       <div className="papaReportes">
-      {listado.map((reporte,index)=>(
-        <div key={index} className='reporteTarjeta'>
+      {reportes.map((reporte,index)=>(
+        <div key={reporte._id} className='reporteTarjeta'>
             <div className="usuarioReporte">
               <img src={usu} alt="imgUsuario" />
-              <h4>Usuario X</h4>
+              <h4>Reporte</h4>
             </div>
             <div className="infoReporte">
                 <label htmlFor="descripcionReporte">{reporte.titulo_reporte}</label>

8d9491f7aeee18bc40320e9722b75d9859cf4d70
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-10-11 19:12:16 -0500

    feat: :bug: Kario principal arreglado

--- frontend/src/pages/Principal.jsx
+++ frontend/src/pages/Principal.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ResponsiveAppBar from '../components/ResponsiveAppBar'
 import Proyectos from '../components/Proyectos'
 import './../assets/css/proyectos.css'
-import { PanelIndicadores } from '../components/PanelIndicadores'
 
 const Principal = () => {
   return (
     <>
         <ResponsiveAppBar/>
-        
         <div className="contenedorProyectos">
-        <PanelIndicadores />
           <Proyectos/>
       </div>
     </>
